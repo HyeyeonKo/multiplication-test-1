fc4ed9df1cb61155b078ccd456e047c9ddef9a48
Hyeyeon <khy33355@khu.ac.kr>
2021-04-15 02:42:24 +0900

    Gugudan

--- multiplication-test.cpp
+++ multiplication-test.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
 	for (int i = 1; i < 10; i++) {
-		for (int j = 1; j <= 10; j++) {
+		for (int j = 1; j <= 9; j++) {
 			cout << i << " * " << j << " = " << i * j << endl;
 		}
 		cout << endl;
